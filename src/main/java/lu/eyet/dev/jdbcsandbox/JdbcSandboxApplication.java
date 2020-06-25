9b24a09df57043639747bf131b0961cb042f24ed
Kai Kremer <kai.kremer@eye-t.local>
2020-06-25 08:19:30 +0200

    change pom to postgres jdbc

--- src/main/java/lu/eyet/dev/jdbcsandbox/JdbcSandboxApplication.java
+++ src/main/java/lu/eyet/dev/jdbcsandbox/JdbcSandboxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class JdbcSandboxApplication {
@@ -14,25 +15,27 @@ public class JdbcSandboxApplication {
 		SpringApplication.run(JdbcSandboxApplication.class, args);
 	}
 
-	/*
-	 * @Bean ApplicationRunner applicationRunner(PetRepository petRepository) {
-	 * return args -> { Pet pet1 = Pet.create("Pfötschen", LocalDate.of(2018, 7,
-	 * 25)); Pet pet2 = Pet.create("Ayiumi", LocalDate.of(2018, 7, 25));
-	 * 
-	 * System.out.println(petRepository.save(pet1));
-	 * System.out.println(petRepository.save(pet2));
-	 * 
-	 * System.out.println(petRepository.findByName("Ayiumi"));
-	 * 
-	 * Address a1 = new Address("2a an der Gruecht"); Customer c1 =
-	 * Customer.create("Kai Kremer", a1); Customer c2 =
-	 * Customer.create("Simone Schram", a1);
-	 * 
-	 * System.out.println(petRepository.save(pet1));
-	 * 
-	 * };
-	 * 
-	 * }
-	 */
+	@Bean
+	@Transactional
+	ApplicationRunner applicationRunner(PetRepository petRepository) {
+		return args -> {
+
+			Pet pet1 = Pet.create("Pfötschen", LocalDate.of(2018, 7, 25));
+			Pet pet2 = Pet.create("Ayiumi", LocalDate.of(2018, 7, 25));
+
+			System.out.println(petRepository.save(pet1));
+			System.out.println(petRepository.save(pet2));
+
+			System.out.println(petRepository.findByName("Ayiumi"));
+
+			Address a1 = new Address("2a an der Gruecht");
+			Customer c1 = Customer.create("Kai Kremer", a1);
+			Customer c2 = Customer.create("Simone Schram", a1);
+
+			System.out.println(petRepository.save(pet1));
+
+		};
+
+	}
 
 }
